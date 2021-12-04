65e23d8dd2336a8a7ab8ee345608c7fee7c06e53
gunasekhar9 <gunasekhar1539@gmail.com>
2021-12-04 11:20:38 +0530

    [gunasekhar9] add : Board Display is done

--- TicTacToe/TicTacToe/GameClass.cs
+++ TicTacToe/TicTacToe/GameClass.cs
@@ -7,7 +7,7 @@ namespace TicTacToe
     public class GameClass
     {
         char[] board = new char[10];
-            public char[] Initial()
+        public char[] Initial()
         {
             for (int i = 1; i < board.Length; i++)
             {
@@ -20,6 +20,18 @@ namespace TicTacToe
             Console.WriteLine("Choose the Charecters 'X' or 'O' for player or for computer to play the Tic Tac Toe Game ");
             char player = Convert.ToChar(Console.ReadLine());
             return player;
+        }
+        public void Display()
+        {
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", board[1], board[2], board[3]);
+            Console.WriteLine("__|_|__ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", board[4], board[5], board[6]);
+            Console.WriteLine("__|_|__ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", board[7], board[8], board[9]);
+            Console.WriteLine("     |     |      ");
         }
     }
 }
