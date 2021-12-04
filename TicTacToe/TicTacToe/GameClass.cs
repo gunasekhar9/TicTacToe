86b1190721d5fd49e4488b81cbd91928b1bf4ca5
gunasekhar9 <gunasekhar1539@gmail.com>
2021-12-04 09:32:53 +0530

    [gunasekhar9] add : TicTacToe

---
+++ TicTacToe/TicTacToe/GameClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    public class GameClass
+    {
+        char[] board = new char[10];
+            public char[] Initial()
+        {
+            for (int i = 0; i < board.Length; i++)
+            {
+                board[i] = ' ';
+            }
+            return board;
+        }
+    }
+}
---
+++ TicTacToe/TicTacToe/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            GameClass game = new GameClass();
+            char[] board = game.Initial();
+        }
+    }
+}
